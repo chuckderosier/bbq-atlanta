d1f65da70b778685ab9f152b459f3b94067f53bd
chuckderosier <chuckderosier2018@gmail.com>
2019-12-08 17:40:50 -0500

    set up App.js and start Home Page

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <div className="title">
+                    <h1>BBQ Atlanta</h1>
+                    <h2>Share and rate BBQ Restaurants in Atlanta</h2>
+                </div>
+                <div className="description">
+                    <p>Description</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
