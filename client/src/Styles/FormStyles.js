2542c5494db1ff06536583cef2742ef2ab7d274a
chuckderosier <chuckderosier2018@gmail.com>
2019-12-10 18:30:57 -0500

    basic styles for forms

---
+++ client/src/Styles/FormStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Form = styled.div`
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    input {
+        margin: .5em auto;
+        width: 40%;
+    }
+    button {
+        margin: 2em auto;
+        padding: 1em;
+    }
+`
--- client/src/components/CreateUserForm.js
+++ client/src/components/CreateUserForm.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Form } from '../Styles/FormStyles'
+import { Title } from '../Styles/TextStyles'
 
 class CreateUserForm extends Component {
     render() {
         return (
             <div>
-                <h1>Create New User</h1>
-                <form>
-                    <input type="text" placeholder="Your Name" />
+                <Title>
+                    <h1>Create New User</h1>
+                </Title>
+                <Form>
+                    <input type="text" placeholder="First Name" />
+                    <input type="text" placeholder="Last Name" />
                     <input type="text" placeholder="Password" />
                     <input type="text" placeholder="Password" />
-                    <button><Link to="/userpage" /></button>
-                </form>
+                    <div></div>
+                    <button><Link to="/userpage">Submit</Link></button>
+                </Form>
             </div>
         )
     }
