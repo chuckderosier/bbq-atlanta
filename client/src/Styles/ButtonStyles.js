ab7aaaab11451f720df9fca1795964cd4f18b518
chuckderosier <chuckderosier2018@gmail.com>
2019-12-12 16:52:45 -0500

    redoing buttons on home page

--- client/src/Styles/ButtonStyles.js
+++ client/src/Styles/ButtonStyles.js
@@ -2,17 +2,26 @@ import styled from 'styled-components'
 
 export const Button = styled.div`
     margin: 0 auto;
-    border-radius: 5em;
+    border-radius: 1em;
     .button {
         color: black;
         text-decoration: none;
     }
-    .button-container {
-        display: flex;
-        justify-content: center;
-    }
 `
 
 export const HomePageButton = styled(Button)`
-
+    
+    background-color: red;
+    .button-container {
+        width: 40%;
+        display: flex;
+        justify-content: center;
+    }
+    .button {
+        margin: .5em auto;
+        width: 5%;
+        height: 5%;
+        color: white;
+        text-align: center;
+    }
 `
