2549c34961f025763674ded9f5264a25774d0856
chuckderosier <chuckderosier2018@gmail.com>
2019-12-15 18:46:02 -0500

    start of NavBar and UserPage

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <div className="left">
+                    <Link to="/"
+                </div>
+                <div className="right"></div>
+            </div>
+        )
+    }
+}
+
+export default NavBar
--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { Title, Text } from '../Styles/TextStyles'
 
 class UserPage extends Component {
     render() {
         return (
-            <div>
-                <h1>User Page</h1>
+            <div className="left">
+                <Title>
+                    <h1>This is 's bbq menu</h1>
+                    <h4>Your preferred BBQ styles:</h4>
+                    <Text></Text>
+                </Title>
             </div>
+            <div className="right"></div>
         )
     }
 }
