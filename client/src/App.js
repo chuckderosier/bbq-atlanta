91476c27b5b548608adfbf398bbd265933d359b3
chuckderosier <chuckderosier2018@gmail.com>
2019-12-19 17:27:09 -0500

    clean up some files

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import Login from './components/Login'
@@ -16,8 +16,8 @@ class App extends Component {
           <Route path='/' component={HomePage} />
         </Switch>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
