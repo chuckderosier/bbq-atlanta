cf20885c5083a5faf7324d8bf75f27be4d2c8fba
chuckderosier <chuckderosier2018@gmail.com>
2019-12-20 18:50:16 -0500

    fixed background image

--- client/src/Styles/BackgroundImageStyle.js
+++ client/src/Styles/BackgroundImageStyle.js
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 export const BackgroundImage = styled.div`
     background-color: white;
     background-image: url("https://i.imgur.com/0MKtHRG.jpg");
+    background-size: cover;
     background-repeat: no-repeat;
-    width: 100vw;
     height: 100vh;
     margin: 0 auto;
 `
