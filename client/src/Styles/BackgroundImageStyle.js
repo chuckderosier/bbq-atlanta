f1f09f19211d1c06b5c97ce59a293c0d7c66e18d
chuckderosier <charlesderosier@hotmail.com>
2020-01-09 17:36:16 -0500

    adjust back-ground image

--- client/src/Styles/BackgroundImageStyle.js
+++ client/src/Styles/BackgroundImageStyle.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const BackgroundImage = styled.div`
     background-color: white;
-    background-image: url("https://i.imgur.com/0MKtHRG.jpg");
+    background-image: url("https://i.imgur.com/7O1CV7F.jpg");
     background-size: cover;
     background-repeat: no-repeat;
     height: 100vh;
