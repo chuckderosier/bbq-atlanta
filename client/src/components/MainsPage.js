9bb362d8d337bc9abc91370f4c8983bd7fb4482e
chuckderosier <charlesderosier@hotmail.com>
2020-02-10 12:04:29 -0500

    added update mains page

--- client/src/components/MainsPage.js
+++ client/src/components/MainsPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Title } from '../Styles/TextStyles'
+import { ClosedMenuStyle, InvisibleBox, CheckBoxStyle } from '../Styles/ContainerStyle'
 
 class MainsPage extends Component {
     render() {
         return (
             <div>
-                <p>Main Page</p>
+                <p>Main Dishes</p>
             </div>
         )
     }
